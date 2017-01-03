0690e00f34626bc4099650290827b50584cdec16
tunnckoCore <mameto_100@mail.bg>
2017-01-03 03:59:06 +0200

    refactor(update): boilerplate & simplify
    drop node < 4, remove few methods, update repo boilerplate
    
    BREAKING CHANGE: drop < 4 support and remove/rename most methods

--- index.js
+++ index.js
@@ -1,174 +1,42 @@
 /*!
  * tmp-file <https://github.com/tunnckoCore/tmp-file>
  *
- * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://i.am.charlike.online)
  * Released under the MIT license.
  */
 
 'use strict'
 
-var gfs = require('graceful-fs')
-var Vinyl = require('vinyl')
-var vinylFile = require('vinyl-file')
-var through2 = require('through2')
-var hybridify = require('hybridify')
-var filepath = require('tmp-filepath')
+const fs = require('fs')
+const tmpFilepath = require('tmp-filepath')
 
-module.exports = tmpFile
+const generateFile = (contents) => {
+  const fp = tmpFilepath('.tmp')
+  /* istanbul ignore next */
+  const data = contents || ''
 
-/**
- * > Creating temporary file and write to disk
- *
- * **Example**
- * ```js
- * var tmpFile = require('tmp-file')
- *
- * var file = tmpFile()
- * console.log(file)
- * //=> { path: '/tmp/cia11kqlt0009tfpvp07inq9f.js',
- * //  contents: <Buffer ...> }
- * ```
- *
- * @param  {Function} `[callback]` optionally pass callback, otherwise it is sync
- * @return {Object} with `.path` and `.contents` properties
- * @api public
- */
-function tmpFile (callback) {
-  var vinyl = tmpFile.generate('.js')
-
-  if (typeof callback !== 'function') {
-    gfs.writeFileSync(vinyl.path, vinyl.contents)
-    return vinyl
+  return {
+    path: fp,
+    contents: data
   }
+}
 
-  gfs.writeFile(vinyl.path, vinyl.contents, function () {
-    callback(null, vinyl)
+const writeFile = (contents) => {
+  return new Promise((resolve, reject) => {
+    const file = generateFile(contents)
+
+    fs.writeFile(file.path, file.contents, (err) => {
+      /* istanbul ignore next */
+      if (err) return reject(err)
+      resolve(file)
+    })
   })
-  return vinyl
 }
 
-/**
- * > Generate virtual file object without writing to disk
- *
- * **Example**
- * ```js
- * var tmpFile = require('tmp-file')
- *
- * var file = tmpFile.generate()
- * console.log(file)
- * //=> { path: '/tmp/cia11kqlt0009tfpvp07inq9f.js',
- * //  contents: <Buffer ...> }
- * ```
- *
- * @param  {String} `[ext]` extension to append to filepath
- * @return {Object} with `.path` and `.contents` properties
- * @api public
- */
-tmpFile.generate = function generate (ext) {
-  var fp = filepath(ext)
-  var data = 'module.exports = "' + fp + '"'
+exports.writeFile = writeFile
+exports.generateFile = generateFile
 
-  return {path: fp, contents: new Buffer(data)}
-}
+writeFile.writeFile = writeFile
+writeFile.generateFile = generateFile
 
-/**
- * > Create Vinyl File object
- *
- * **Example**
- * ```js
- * var tmpFile = require('tmp-file')
- *
- * var file = tmpFile.vinyl()
- * console.log(file)
- * //=> <File "../../../../tmp/cia11zwn00000mjpvom4ioitr.js" <Buffer ... >>
- * ```
- *
- * @return {Object|Vinyl} object that is compitable with Vinyl File Format
- * @api public
- */
-tmpFile.vinyl = function tmpVinyl () {
-  var vinyl = tmpFile.generate('.js')
-  return new Vinyl(vinyl)
-}
-
-/**
- * > Create temp file as stream and write it to disk
- *
- * **Example**
- * ```js
- * var tmpFile = require('tmp-file')
- * var stream = tmpFile.stream()
- *
- * stream
- * .pipe(through2.obj(function (file, enc, next) {
- *   console.log(file)
- *   //=> { path: '/tmp/cia123v5d0004u7pvfa01sjq9.js',
- *   //  contents: <Buffer ...> }
- *   next()
- * })
- * ```
- *
- * @return {Stream} through2 stream in object mode, can be used in gulp
- * @api public
- */
-tmpFile.stream = function tmpStream () {
-  var vinyl = tmpFile()
-  return gfs.createReadStream(vinyl.path).pipe(through2.obj(function () {
-    this.push(vinyl)
-  }))
-}
-
-/**
- * > Create temp file as gulp and write it to disk
- *
- * **Example**
- * ```js
- * var tmpFile = require('tmp-file')
- * var gulp = tmpFile.gulp()
- *
- * gulp
- * .pipe(through2.obj(function (file, enc, next) {
- *   console.log(file)
- *   //=> <File "../../../../tmp/cia1arvm000066rpv0dqfzy7u.js" <Buffer ... >>
- *   next()
- * })
- * ```
- *
- * @return {Stream} through2 gulp in object mode, can be used in gulp
- * @api public
- */
-tmpFile.gulp = function tmpGulp () {
-  var vinyl = tmpFile()
-  return gfs.createReadStream(vinyl.path).pipe(through2.obj(function () {
-    var file = vinylFile.readSync(vinyl.path)
-    this.push(file)
-  }))
-}
-
-/**
- * > Create hybrid temp file with hybridify
- *
- * **Example**
- * ```js
- * var tmpFile = require('tmp-file')
- * var hybrid = tmpFile.promise()
- *
- * hybrid(function (err, file) {
- *   console.log(file)
- *   //=> { path: '/tmp/cia128gmg000721pvndiq2d53.js',
- *   //  contents: <Buffer ...> }
- * })
- * .then(function (file) {
- *   console.log(file)
- *   //=> { path: '/tmp/cia128gmg000721pvndiq2d53.js',
- *   //  contents: <Buffer ...> }
- * })
- * .catch(console.error)
- * ```
- *
- * @return {Promise} promise/hybrid
- * @api public
- */
-tmpFile.promise = function tmpPromise () {
-  return hybridify(tmpFile)
-}
+module.exports = exports['default'] = writeFile
--- test.js
+++ test.js
@@ -1,180 +1,27 @@
 /*!
  * tmp-file <https://github.com/tunnckoCore/tmp-file>
  *
- * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://i.am.charlike.online)
  * Released under the MIT license.
  */
 
-/* jshint asi:true */
-
 'use strict'
 
-var gfs = require('graceful-fs')
-var test = require('assertit')
-var isPromise = require('is-promise')
-var isStream = require('is-stream')
-var through2 = require('through2')
-var osTmpdir = require('os-tmpdir')
-var tmpFile = require('./index')
+const test = require('mukla')
+const rimraf = require('rimraf')
+const tmpFile = require('./index')
 
-function isFileSync (fp) {
-  try {
-    return gfs.statSync(fp).isFile()
-  } catch(err) {
-    return false
-  }
-}
+test('should just generate file object', (done) => {
+  const actual = tmpFile.generateFile('foobar')
+  test.strictEqual(actual.contents, 'foobar')
+  done()
+})
 
-test('tmp-file:', function () {
-  test('should return real vinyl file object on .vinyl()', function (done) {
-    var tmp = tmpFile.vinyl()
-    var actual = tmp.path.indexOf(osTmpdir()) !== -1
-    var expected = true
-
-    var exist = isFileSync(tmp.path)
-
-    test.ok(tmp.path, 'should have .path property')
-    test.ok(tmp.contents, 'should have .contents property')
-    test.equal(actual, expected, 'should have /tmp at returned .path')
-    test.equal(exist, false, 'should file not exist on disk')
+test('should return promise with created file on disk', (done) => {
+  tmpFile.writeFile('quix').then((file) => {
+    test.strictEqual(file.contents, 'quix')
+    test.strictEqual(/tmp/.test(file.path), true)
+    rimraf.sync(file.path)
     done()
-  })
-
-  test('should write file to disk and return .path and .contents (sync)', function (done) {
-    var tmp = tmpFile()
-    var actual = isFileSync(tmp.path)
-    var expected = true
-
-    test.ok(tmp.path, 'should have .path property')
-    test.ok(tmp.contents, 'should have .contents property')
-    test.equal(actual, expected, 'should filepath exists')
-    done()
-  })
-
-  test('should async write file to disk and have .path and .contents', function (done) {
-    tmpFile(function (err, tmp) {
-      var isFile = isFileSync(tmp.path)
-      var actual = tmp.path.indexOf(osTmpdir()) !== -1
-      var expected = true
-
-      test.ifError(err, 'should be null')
-      test.ok(tmp.path, 'should have .path property')
-      test.ok(tmp.contents, 'should have .contents property')
-      test.equal(isFile, true, 'should filepath exists')
-      test.equal(actual, expected, 'should have /tmp at returned .path')
-      done()
-    })
-  })
-
-  test('should be (readable) stream when .stream() or .gulp()', function (done) {
-    var stream = tmpFile.stream()
-    var gulp = tmpFile.gulp()
-
-    test.equal(isStream(stream), true, 'should be stream')
-    test.equal(isStream(gulp), true, 'should be stream')
-    test.equal(isStream.readable(stream), true, 'should be readable stream')
-    done()
-  })
-
-  test('should be readable stream in object mode when .stream()', function (done) {
-    var stream = tmpFile.stream()
-
-    stream
-    .pipe(through2.obj(function assertPlugin (file, enc, next) {
-      var actual = file.path.indexOf(osTmpdir()) !== -1
-      var expected = true
-
-      var exist = isFileSync(file.path)
-
-      test.ok(file.path, 'should have .path property')
-      test.ok(file.contents, 'should have .contents property')
-      test.equal(actual, expected, 'should have /tmp at returned .path')
-      test.equal(exist, true, 'should file exist on disk')
-      done()
-    }))
-    .on('error', done)
-  })
-
-  test('should be transform stream in object mode for Gulp, using .gulp()', function (done) {
-    var gulp = tmpFile.gulp()
-
-    gulp
-    .pipe(through2.obj(function assertPlugin (file, enc, next) {
-      var exist = isFileSync(file.path)
-
-      test.ok(exist)
-      test.ok(file.cwd === process.cwd())
-      test.ok(file.base === process.cwd())
-      test.ok(file.path)
-      test.ok(typeof file.stat === 'object')
-      test.ok(Buffer.isBuffer(file.contents))
-      test.ok(file.contents.length > 10)
-      done()
-    }))
-    .on('error', done)
-  })
-
-  test('should be promise and hybrid when .promise()', function (done) {
-    var hybrid = tmpFile.promise()
-    var actual = isPromise(hybrid())
-    var expected = true
-
-    test.equal(actual, expected, 'should be promise')
-    test.equal(typeof hybrid.hybridify, 'function', 'should be hybrid')
-    test.equal(typeof hybrid().hybridify, 'function', 'should be hybrid')
-    done()
-  })
-
-  test('should promise/hybrid have .path and .contents with .promise()', function (done) {
-    var hybrid = tmpFile.promise()
-
-    hybrid()
-    .then(function (file) {
-      var actual = file.path.indexOf(osTmpdir()) !== -1
-      var expected = true
-
-      test.ok(file.path, 'should have .path property')
-      test.ok(file.contents, 'should have .contents property')
-      test.equal(actual, expected, 'should have /tmp at returned .path')
-      done()
-    })
-    .catch(done)
-  })
-
-  test('should file exist on disk when .promise() with callback style', function (done) {
-    var hybrid = tmpFile.promise()
-    var cnt = 0
-
-    hybrid(function (err, file) {
-      var actual = isFileSync(file.path)
-      var expected = true
-
-      test.ifError(err, 'should be null')
-      test.equal(actual, expected, 'should file exist on disk')
-      cnt++
-    })
-    .then(function () {
-      cnt++
-      test.equal(cnt, 2, 'should be able to work as callback and promise')
-      done()
-    })
-    .catch(done)
-  })
-
-  test('should have .generate([ext]) method and not write file to disk', function (done) {
-    var vinyl = tmpFile.generate()
-    var vinylJsFile = tmpFile.generate('.js')
-
-    var actual = vinyl.path.indexOf(osTmpdir()) !== -1
-    var expected = true
-
-    var exist = isFileSync(vinyl.path)
-
-    test.ok(vinyl.path, 'should have .path property')
-    test.ok(vinyl.contents, 'should have .contents property')
-    test.equal(actual, expected, 'should have /tmp at returned .path')
-    test.equal(exist, false, 'should not write file to disk')
-    test.ok(vinylJsFile.path)
-    done()
-  })
+  }).catch(done)
 })
